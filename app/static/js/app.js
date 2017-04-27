d43e3b8b98e2d9231788351ed24923ab347fcbb3
Jhanelle Thomas <jhanelle.thomas02@mymona.uwi.edu>
2017-04-27 23:53:47 +0000

    Finished Select thumbnails option

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -8,14 +8,19 @@ app.controller("imageCtrl", function($scope, $http){
             $scope.images = response.data["thumbnails"];
             
             var i=0;
-            var htm = "";
+            var htm = "<p>Select A Thumbnail Below</p><br/>";
             var loc = document.getElementById("pics");
             
             for (i=0; i < $scope.images.length; i++){
-                htm += "<img src="+ '"'+ $scope.images[i] +'"' +"/>";
+                htm += "<img src="+ '"'+ $scope.images[i] +'"'+"onclick="+ '"giveThumbail(this)"'+"/>";
             }
             
             loc.innerHTML = htm;
         });
     };
 });
+
+function giveThumbail(img){
+        var loc = document.getElementById("thumbnail");
+        loc.value = img.src;
+}
