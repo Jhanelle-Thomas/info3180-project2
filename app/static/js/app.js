d3c1e33b00603ac0241511a4c0a883f75b51fd74
Jhanelle Thomas <jhanelle.thomas02@mymona.uwi.edu>
2017-04-27 23:39:48 +0000

    Added get thumbnails

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -2,7 +2,20 @@
 var app = angular.module("wishListApp", []);
 
 app.controller("imageCtrl", function($scope, $http){
-    $http.get('/api/thumbnails').then(function(response){
-        $scope.images = response.data["thumbnails"];
-    });
+    
+    $scope.getImages = function(){
+        $http.get('/api/thumbnails',{params:{"url": $scope.url}}).then(function(response){
+            $scope.images = response.data["thumbnails"];
+            
+            var i=0;
+            var htm = "";
+            var loc = document.getElementById("pics");
+            
+            for (i=0; i < $scope.images.length; i++){
+                htm += "<img src="+ '"'+ $scope.images[i] +'"' +"/>";
+            }
+            
+            loc.innerHTML = htm;
+        });
+    };
 });
